36cbfdd31e470c9ad977b36db91f0e7955b64d11
wmann95 <william.d.mann95@gmail.com>
2021-04-28 19:21:59 -0400

    Early commit to amend in other branch.

---
+++ HCFarming/src/gui/GUI.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HCFarming.src.gui
+{
+	abstract class GUI
+	{
+
+		public Vector2 position { get; private set; }
+
+		public GUI()
+		{
+
+		}
+
+		public void onDraw()
+		{
+
+		}
+
+	}
+}
---
+++ HCFarming/src/gui/GUIButton.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HCFarming.src.gui
+{
+	class GUIButton
+	{
+	}
+}
